65257b92f5e46da791ddd29c44e4dc325cc367ed
Lucas <lucasidelfonso73@gmail.com>
2022-11-17 21:46:03 -0300

    - Finalizing routes and generating data for testing - Starting models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length =65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_unit = models.CharField()
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True) #Ele pega a data atual e não muda mais
+    updated_at = models.DateField(auto_now=False) #Ele vai atualizando a data sempre que é chamado
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 def home(request):
     return render(request,'recipes/pages/home.html', context={
-        'name': 'Lucas Vinicius'
+        'recipes': [make_recipe() for _ in range(12)]
     })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True
+    })
 
 def contact(request):
     return HttpResponse('My contact')
